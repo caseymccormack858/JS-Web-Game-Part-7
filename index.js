cf27bc3b119a7d4ed2b6049d89fdb833153e80be
Casey McCormack <caseymccormack858@gmail.com>
2023-07-21 02:38:42 -0700

    updated revised asych await final

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-async function moveNPC() {
-    console.log('Start walking east');
-    await npc.walkEast(2000);
-    console.log('Finished walking east');
-    console.log('Start walking south');
-    await npc.walkSouth(300);
-    console.log('Finished walking south');
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
 }
+
+    
 moveNPC ()
 // Create the inventory
 const inventory = newInventory()
